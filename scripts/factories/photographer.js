7951779efb16225ba0d68238b76f22d5140c7f66
Frederic.K <frederic.kreuder@gmail.com>
2022-11-28 15:40:26 +0100

    Layout caption

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,54 +1,63 @@
 function photographerFactory(data) {
-    const { portrait, name, city, country } = data;
+    const { portrait, name, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
-    const link = "www.google.com";
+    const link = "#";
 
     function getUserCardDOM() {
 
-        const photographerLink = document.createElement( 'a' );
-        photographerLink.classList.add("photographerLink");
+        const photographerArticleCard = document.createElement("article");
+        photographerArticleCard.classList.add("photographer__article");
+        
+        const photographerLink = document.createElement("a");
+        photographerLink.classList.add("photographer__article--link");
         photographerLink.setAttribute("href", link);
 
+        const photographerImgLinkCard = document.createElement("div");
+        photographerImgLinkCard.classList.add("photographer__article--linkImg")
 
-        const article = document.createElement( 'article' );
-        article.classList.add("photographer__article");
+        const photographerImgCard = document.createElement("img");
+        photographerImgCard.classList.add("photographer__article--img");
+        photographerImgCard.setAttribute("src", picture);
+
+        const photographerNameCard = document.createElement("h2");
+        photographerNameCard.classList.add("photographer__article--linkTitle");
+        photographerNameCard.textContent = name;
         
-        const photographerArticleCaption = document.createElement( 'div' );
+        const photographerArticleCaption = document.createElement("div");
         photographerArticleCaption.classList.add("photographer__article--caption");
 
-        const photographerArticleImg = document.createElement( 'div' );
-        photographerArticleImg.classList.add("photographer__article--captionImg");
-
-        const img = document.createElement( 'img' );
-        img.classList.add("photographer__article--img");
-        img.setAttribute("src", picture);
-
-        const h2 = document.createElement( 'h2' );
-        h2.classList.add("photographer__article--captionTitle");
-        h2.textContent = name;
-
-        const photographerlocation = document.createElement('div');
+        const photographerlocation = document.createElement("div");
         photographerlocation.classList.add("photographer__article--captionLocation");
 
-        const photographerlocationCity = document.createElement('p');
+        const photographerlocationCity = document.createElement("p");
         photographerlocationCity.classList.add("photographer__article--captionCity");
         photographerlocationCity.textContent = city;
 
-        const photographerlocationCountry = document.createElement('p');
+        const photographerlocationCountry = document.createElement("p");
         photographerlocationCountry.classList.add("photographer__article--captionCountry");
         photographerlocationCountry.textContent = country;
 
-        photographerLink.appendChild(article);
-        article.appendChild(photographerArticleCaption);
-        photographerArticleCaption.appendChild(photographerArticleImg);
-        photographerArticleImg.appendChild(img);
-        photographerArticleCaption.appendChild(h2);
+        const photographTagLine = document.createElement("p");
+        photographTagLine.classList.add("photographer__article--captionTagLine");
+        photographTagLine.textContent = tagline;
+
+        const photographerPrice = document.createElement("p");
+        photographerPrice.classList.add("photographer__article--captionPrice");
+        photographerPrice.textContent = price;
+
+        photographerArticleCard.appendChild(photographerLink);
+        photographerLink.appendChild(photographerImgLinkCard)
+        photographerImgLinkCard.appendChild(photographerImgCard);
+        photographerLink.appendChild(photographerNameCard);
+        photographerArticleCard.appendChild(photographerArticleCaption)
         photographerArticleCaption.appendChild(photographerlocation);
         photographerlocation.appendChild(photographerlocationCity);
         photographerlocation.appendChild(photographerlocationCountry);
+        photographerArticleCaption.appendChild(photographTagLine);
+        photographerArticleCaption.appendChild(photographerPrice);
 
-        return (photographerLink);
+        return (photographerArticleCard);
     }
     return { picture, name, getUserCardDOM }
 };
