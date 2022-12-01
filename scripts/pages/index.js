5f6464f6c0b519139a6e66508e8eea8057fed26a
Frederic.K <frederic.kreuder@gmail.com>
2022-12-01 13:52:56 +0100

    Clean up the "mess"

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,51 +1,44 @@
-    //async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-       /* const photographers = [
-            {
-                "name": "Mimi Keel",
-                "id": 243,
-                "city": "London",
-                "country": "UK",
-                "tagline": "Voir le beau dans le quotidien",
-                "price": 400,
-                "portrait": "MimiKeel.jpg"
-            },
-            {
-                "name": "Ellie-Rose Wilkens",
-                "id": 930,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Capturer des compositions complexes",
-                "price": 250,
-                "portrait": "EllieRoseWilkens.jpg"
-            },
-            {
-                "name": "Tracy Galindo",
-                "id": 82,
-                "city": "Montreal",
-                "country": "Canada",
-                "tagline": "Photographe freelance",
-                "price": 500,
-                "portrait": "TracyGalindo.jpg"
-            },
-        ]*/ 
-        /* et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    };*/
-    
-    /*async function getPhotographers () {
-        const photographersData = fetch('data/photographers.json')
+
+// Fonction Fetch 
+   async function getPhotographersData() {
+        const photographersData = await fetch("data/photographers.json")
         .then(function(response) {
+            if (response.ok) {
                 return response.json()
+            }
+            else {
+                console.log("Une erreur réseau est survenue")
+            }
         })
-        /*.then(function(photographersData) {
+        .then(function(photographersData) {
             return photographersData
         })
+        .catch(function(error) {
+            console.log("Une erreur est survenue avec l'opération fetch")
+        })
         return photographersData
-    };*/
+    };
+    
+    async function displayData(photographers) {
+        const photographersSection = document.querySelector(".photographer__section");
 
-    // Fonction Fetch 
+        photographers.forEach((photographer) => {
+            const userCardDOM = getPhotographerCardDOM(photographer);
+            photographersSection.appendChild(userCardDOM);
+        });
+    };
+
+    async function init() {
+        const { photographers } = await getPhotographersData();
+        displayData(photographers);
+    };
+
+    init();
+
+
+
+
+        // Fonction Fetch 
    /* async function getPhotographers() {
         const photographersData = await fetch("data/photographers.json")
         .then(function(response) {
@@ -87,36 +80,18 @@
         return photographersData
     };*/
 
-    
-    async function getPhotographers() {
+   /* async function getPhotographers() {
         const photographersData = await fetch("data/photographers.json")
         .then ((response) => {
             if (response.ok) {
                 return response.json();
             }
             else {
-                console.log("Une erreur réseau est survenue");
+               return console.log("Une erreur réseau est survenue");
             }
         })
         .catch(function(error) {
-            console.log("Une erreur est survenue avec l'opération fetch")
+            return console.log("Une erreur est survenue avec l'opération fetch")
         })
         return photographersData
-    };
-
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer__section");
-
-        photographers.forEach((photographer) => {
-            const userCardDOM = getPhotographerCardDOM(photographer);
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
-
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
-
-    init();
+    };*/
