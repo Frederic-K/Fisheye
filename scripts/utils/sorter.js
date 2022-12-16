f8564c330acc28b6ea9eb727a0a78ae8122cd0ea
Frederic.K <frederic.kreuder@gmail.com>
2022-12-16 10:15:12 +0100

    Fix sorter menu position + add js rules to hide nd schow sorter

--- scripts/utils/sorter.js
+++ scripts/utils/sorter.js
@@ -11,15 +11,15 @@
     sorterDropdown.appendChild(sorterDropdownMenu);
 
     const sorterDropdownBtn = document.createElement("div");
-    sorterDropdownBtn.classList.add("dropdown__button", "selected");
+    sorterDropdownBtn.classList.add("dropdown__button");
     sorterDropdownBtn.setAttribute("role", "button");
     sorterDropdownBtn.setAttribute("aria-expanded", "false");
     sorterDropdownBtn.textContent = "Popularité";
     sorterDropdownMenu.appendChild(sorterDropdownBtn);
 
     const sorterDropdownBtnIcon = document.createElement("div");
-    sorterDropdownBtnIcon.classList.add("dropdown__button--icon");
-    sorterDropdownBtn.appendChild(sorterDropdownBtnIcon);
+    sorterDropdownBtnIcon.classList.add("dropdown__icon");
+    sorterDropdown.appendChild(sorterDropdownBtnIcon);
 
     const sorterDropdownIconChevron = document.createElement("i");
     sorterDropdownIconChevron.classList.add("fa-solid", "fa-chevron-down");
@@ -41,15 +41,44 @@
     dropdownSortByTitle.textContent = "Titre";
     sorterDropdownContent.appendChild(dropdownSortByTitle);
     
-
+    const dropdownContent = document.getElementsByClassName("dropdown__content")[0];
 
 sorterDropdownBtn.addEventListener("click", () => dropdownMenu());
 
 function dropdownMenu() {
-    const dropdownContent = document.getElementsByClassName("dropdown__content")[0];
+    //const dropdownContent = document.getElementsByClassName("dropdown__content")[0];
     dropdownContent.classList.toggle("hidden");
     //sorterDropdownBtnIcon.classList.toggle("arrow-animation-down");
     sorterDropdownBtnIcon.classList.toggle("arrow-animation-up");
 };
+
+/*dropdownSortByFamous.addEventListener("click", () => {
+    dropdownMenu();
+    sorterDropdownBtn.textContent = "Popularité";
+});*/
+
+dropdownSortByDate.addEventListener("click", () => {
+    dropdownMenu();
+    dropdownSortByTitle.classList.remove("selected", "hidden");
+    dropdownSortByFamous.classList.remove("selected", "hidden");
+    dropdownSortByDate.classList.add("selected", "hidden");
+    sorterDropdownBtn.textContent = document.getElementsByClassName("selected")[0].textContent;
+});
+
+dropdownSortByTitle.addEventListener("click", () => {
+    dropdownMenu();
+    dropdownSortByDate.classList.remove("selected", "hidden");
+    dropdownSortByFamous.classList.remove("selected", "hidden");
+    dropdownSortByTitle.classList.add("selected", "hidden");
+    sorterDropdownBtn.textContent = document.getElementsByClassName("selected")[0].textContent;
+});
+
+dropdownSortByFamous.addEventListener("click", () => {
+    dropdownMenu();
+    dropdownSortByDate.classList.remove("selected", "hidden");
+    dropdownSortByTitle.classList.remove("selected", "hidden");
+    dropdownSortByFamous.classList.add("selected", "hidden");
+    sorterDropdownBtn.textContent = document.getElementsByClassName("selected")[0].textContent;
+});
 
 
