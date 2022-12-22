e4ff1d818edd3ff8fc909a40b65cf51cb5d77edb
Frederic.K <frederic.kreuder@gmail.com>
2022-12-22 13:13:58 +0100

    Testing trap focus in modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -3,8 +3,12 @@ function displayModal() {
 
     const modal = document.getElementById("contact_modal");
     modal.style.display = "block";
-    modal.setAttribute("role", "dialog")
-    modal.setAttribute("aria-hidden", "false");
+
+    const modalContainer = document.getElementsByClassName("modal")[0];
+    modalContainer.setAttribute("role", "dialog")
+    modalContainer.setAttribute("aria-hidden", "false");
+    modalContainer.setAttribute("aria-modal", "true");
+    modalContainer.setAttribute("aria-controls", "modal");
     
     // TODE revoir le aria-describedby : 
     //modal.setAttribute("aria-describedby", "modalHeaderTitle");
@@ -25,10 +29,10 @@ function displayModal() {
     const modalHeaderImgESC = document.getElementsByClassName("modal__header--imgESC")[0];
     modalHeaderImgESC.setAttribute("alt", "Bouton de fermeture du formulaire de contact");
     modalHeaderImgESC.setAttribute("aria-label", "Bouton de fermeture du formulaire de contact")
-    modalHeaderImgESC.setAttribute("tabindex", "6");
+    modalHeaderImgESC.setAttribute("tabindex", "");
     modalHeaderImgESC.addEventListener("click", () => closeModal());
 
-    const modalFormContainer = document.getElementsByClassName("modal__form--container")[0];
+    //const modalFormContainer = document.getElementsByClassName("modal__form--container")[0];
 
     /*const modalFormLabelPrenom = document.createElement("label");
     modalFormLabelPrenom.classList.add("modal__form--label");
@@ -117,14 +121,112 @@ function displayModal() {
     };
 
     document.getElementById("firstname").focus();
-};
+
+    /////////////////////////////////////////////////////////////////////
+
+    /*const focusableElements = 'input, [tabindex]:not([tabindex="-1"]), img, button';
+    console.log("focusableElements", focusableElements);*/
+
+      keepFocus = function (context) {
+        //const allFocusableElements = context.querySelectorAll(focusableElements);
+        //console.log("allFocusableElements", allFocusableElements)
+        //let firstFocusableElement = allFocusableElements[0];
+        //console.log("firstFocusableElement", firstFocusableElement);
+        //let lastFocusableElement = allFocusableElements[allFocusableElements.length - 1];
+        //console.log("lastFocusableElement", lastFocusableElement);
+
+        /*const firstFocusableElement = document.getElementById("firstname");
+        const lastFocusableElement = document.getElementsByClassName("modal__form--sendBtn")[0];
+
+        keyListener = function (event) {
+            let keyCode = event.which || event.keyCode; // Get the current keycode
+            console.log("keyCode", keyCode);
+            // Polyfill to prevent the default behavior of events
+            event.preventDefault = event.preventDefault || function () {
+                event.returnValue = false;
+            };
+
+            // If it is TAB
+            if (keyCode === 9) {
+
+                // Move focus to first element that can be tabbed if Shift isn't used
+                if (event.target === lastFocusableElement && !event.shiftKey) {
+                    event.preventDefault();
+                    firstFocusableElement.focus();
+                    console.log("K9firstFocusableElement", firstFocusableElement);
+
+                // Move focus to last element that can be tabbed if Shift is used
+                } else if (event.target === firstFocusableElement && event.shiftKey) {
+                    event.preventDefault();
+                    lastFocusableElement.focus();
+                    console.log("K9lastFocusableElement", lastFocusableElement);
+                }
+            }
+        };
+        
+        context.addEventListener('keydown', keyListener, false);
+    };
+    // Call the function when the part of the page gets focus
+    //keepFocus(modal);
+    keepFocus(modalContainer);
+    //modal.focus();
+    modalContainer.focus();*/
+
+    /////////////////////////////////////////////////////////////////////
+
+    // add all the elements inside modal which you want to make focusable
+    /*const  focusableElements =
+    'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+    const modaltest = document.querySelector('#modalContainer'); // select the modal by it's id
+    console.log("modaltest", modaltest);
+
+    const firstFocusableElement = modaltest.querySelectorAll(focusableElements)[0]; // get first element to be focused inside modal
+    console.log("firstFocusableElement", firstFocusableElement);
+    const focusableContent = modaltest.querySelectorAll(focusableElements);
+    console.log("focusableContent", focusableContent);
+    const lastFocusableElement = focusableContent[focusableContent.length - 1]; // get last element to be focused inside modal
+    console.log("lastFocusableElement", lastFocusableElement);*/
+
+    const firstFocusableElement = document.getElementById("firstname");
+    const lastFocusableElement = document.getElementsByClassName("modal__form--sendBtn")[0];
+
+
+    document.addEventListener('keydown', function(e) {
+        let isTabPressed = e.key === 'Tab';
+
+            if (!isTabPressed) {
+                return;
+            }
+
+        if (e.shiftKey) { // if shift key pressed for shift + tab combination
+            if (document.activeElement === firstFocusableElement) {
+                lastFocusableElement.focus(); // add focus for the last focusable element
+                console.log("K9lastFocusableElement", lastFocusableElement);
+                e.preventDefault();
+                }
+            } else { // if tab key is pressed
+            if (document.activeElement === lastFocusableElement) { // if focused has reached to last focusable element then focus first focusable element after pressing tab
+                firstFocusableElement.focus(); // add focus for the first focusable element
+                console.log("K9firstFocusableElement", firstFocusableElement);
+                e.preventDefault();
+                }
+            }
+        });
+
+    firstFocusableElement.focus();
+    console.log("FinalfirstFocusableElement", firstFocusableElement); 
+
+    //////////////////////////////////////////////////////////////////////
+    }
+}
 
 function closeModal() {
     const main = document.getElementById("main");
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
-    modal.setAttribute("aria-hidden", "true");
+    const modalContainer = document.getElementsByClassName("modal")[0];
+    modalContainer.setAttribute("aria-hidden", "true");
+    modalContainer.setAttribute("aria-modal", "false");
     main.setAttribute("aria-hidden", "false");
 };
-
 
